4ae0f3ac197e223e1b1a6a75ec6b33100f1e457c
Flowmar <omar.imam@kineticvortex.com>
2017-07-18 05:00:17 -0500

    add morgan and js

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php header( 'Location: public/home.html' ) ;  ?>
+<?php header( 'Location: public/index.html' ) ;  ?>
--- server.js
+++ server.js
@@ -1,20 +1,24 @@
+// Dependencies
+
 var express = require("express");
 var expHbs = require("express-handlebars");
 var mongoose = require("mongoose");
+var logger = require('morgan');
 var bodyParser = require("body-parser");
 var cheerio = require("cheerio");
 var request = require("request");
 var path = require('path');
 
+// Create instance of express
 var app = express();
-const PORT = 3030;
+const PORT = 3000;
 
 const MONGODB_URI = "mongodb://heroku_8fpz7mpz:rhnm9h6727pte92f8cvge94nsb@ds157342.mlab.com:57342/heroku_8fpz7mpzs"
 
+// Tell express to use the directory 'public' to serve static files from
+app.use(express.static('public'));
+
+
+// Tell the app to listen on the selected port.
 app.listen(PORT);
 console.log("App is listening on port: ", PORT);
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/public/home.html'));
-});
-
